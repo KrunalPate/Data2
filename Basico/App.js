740c0467a6233ca54ee4f942244e13f7e60ee4f9
Krunal Patel <patel.krunal.tech@gmail.com>
2024-07-23 14:24:08 +0000

    feat: made some changes

---
+++ Basico/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import BoasVindas from './components/BoasVindas';
+import Calculos from './components/Calculos';
+
+export default class App extends Component{
+  render(){
+    return (
+      <View style={styles.container}>
+        <BoasVindas saudacao='Bom dia!' />
+        <Calculos/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+     flex: 1,
+     justifyContent: "center",
+     alignItems: "center",
+     backgroundColor: '#757575',
+  },
+});
